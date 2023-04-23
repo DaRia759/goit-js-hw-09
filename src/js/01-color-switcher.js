e9da1060c383c4f572f2101f712cb79e4a31f18b
DaRia759 <117763073+DaRia759@users.noreply.github.com>
2023-04-23 21:07:53 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startButton = document.getElementsByTagName('.button');
+const stopButton = document.getElementsByTagName('.button');
 
+let colorID = null; 
+
+startButton.addEventListener('click', () => {
+    colorID = setInterval(() => {
+        document.body.style.backgroundColor = `${getRandomHexColor()}`;
+    }, 1000);
+
+    startButton.disabled = true;
+});
+
+stopButton.addEventListener('click', () => {
+    clearInterval(colorID);
+    stopButton.disabled = false;
+});
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+};
